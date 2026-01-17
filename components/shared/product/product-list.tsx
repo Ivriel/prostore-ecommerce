4d100047b07188397bcfd276f0efd5e04f7db53d
Ivriel <gunawanivriel@gmail.com>
2026-01-17 11:26:18 +0700

    Product card component

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 const ProductList = ({
   data,
   title,
@@ -12,9 +14,9 @@ const ProductList = ({
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
           {limitedData.map((product: any) => (
-            <div>{product.name}</div>
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
