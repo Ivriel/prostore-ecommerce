f09dc0a11d6cf27ad7743a8a6c8ee97d4afe51d6
Ivriel <gunawanivriel@gmail.com>
2026-01-17 11:15:20 +0700

    Sample products and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve,ms) )
 
 const Homepage = async () => {
   // await delay(2000);
-  return <>Prostore</>;
-}
- 
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrival"
+        limit={4}
+      />
+    </>
+  );
+};
+
 export default Homepage;
