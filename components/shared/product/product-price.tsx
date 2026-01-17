e9f9443ab78b1c0050f2c48ae8a935bf385d7c39
Ivriel <gunawanivriel@gmail.com>
2026-01-17 11:40:33 +0700

    product price component

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  // ensure two decimal places
+  const stringValue = value.toFixed(2);
+  // get int/float
+  const [intValue, floatValues] = stringValue.split(".");
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValues}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
